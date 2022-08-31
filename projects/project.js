18fad9ee876dc7f03690b290518162a88b4faa47
Alex Pushkin <apushkaa@gmail.com>
2022-08-31 21:27:18 +0300

    transition fixed

--- projects/project.js
+++ projects/project.js
@@ -4,11 +4,12 @@ const modal = document.querySelector(".modal");
 images.forEach((image) => {
   image.addEventListener("mouseover", () => {
     modal.children[0].src = image.getAttribute("src");
-    modal.style.transition = "opacity 0.3s ease-in-out";
+    modal.style.transition = "opacity 0.3s ease-out";
     modal.style.opacity = "1";
     document.body.style.overflow = "hidden";
   });
   image.addEventListener("mouseout", () => {
+    modal.style.transition = "none";
     modal.style.opacity = "0";
     document.body.style.overflow = "visible";
     modal.children[0].src = "";
